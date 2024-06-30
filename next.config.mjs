f7fb5162d6e35b41cf32e5a87617e08d0294112e
Seonyeong Jang <ssoju2827@gmail.com>
2024-07-01 00:29:48 +0900

    separate build and touch nojekyll

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,11 @@
-/**
-* @type {import('next').NextConfig}
-*/
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
+    output: 'export',
     images: {
-      loader: "akamai",
-      path: "",
+      unoptimized: true
     },
-    assetPrefix: "./",
-  };
+    // 만약 GitHub Pages에서 서브디렉토리에 배포한다면 아래 줄의 주석을 제거하고 적절히 수정하세요
+    // basePath: '/your-repo-name',
+  }
   
-  export default nextConfig;
+  module.exports = nextConfig
