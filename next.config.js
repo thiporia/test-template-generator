7d3375ef4b24f2e1b8ab3402eb23f6639cc7bce8
Seonyeong Jang <ssoju2827@gmail.com>
2024-06-30 22:57:57 +0900

    package: eslint downgrade v9 -> v8

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
-const nextConfig = {};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+};
 
-export default nextConfig;
+module.exports = nextConfig;
