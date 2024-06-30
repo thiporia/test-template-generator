32eadf12c110e18a633ec05f857446bbdcacf263
Seonyeong Jang <ssoju2827@gmail.com>
2024-07-01 01:23:30 +0900

    next config assetPrefix condition

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
+const isProduction = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
-  images: {
-    unoptimized: true
-  },
+  assetPrefix: isProduction ? '/test-template-generator' : '/',
 }
 
 module.exports = nextConfig
