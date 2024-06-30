f39e167f6ca2af35610d0c5d050d79aafcc8d5d0
Seonyeong Jang <ssoju2827@gmail.com>
2024-07-01 00:58:55 +0900

    deploy folder .next to out

--- next.config.mjs
+++ next.config.mjs
@@ -4,10 +4,8 @@ const nextConfig = {
     basePath: '/test-template-generator',
     assetPrefix: '/test-template-generator/',
     images: {
-      unoptimized: true
+        unoptimized: true
     },
-    // 만약 GitHub Pages에서 서브디렉토리에 배포한다면 아래 줄의 주석을 제거하고 적절히 수정하세요
-    // basePath: '/your-repo-name',
-  }
-  
-  module.exports = nextConfig
+}
+
+module.exports = nextConfig
