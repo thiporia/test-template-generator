ac178260ca4bbb56d58aa7f03ee62d2cf0b216f3
Seonyeong Jang <ssoju2827@gmail.com>
2024-07-02 00:12:21 +0900

    env files

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
-
 const nextConfig = {
-  basePath,
-  assetPrefix: basePath,
   output: 'export',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
   images: {
     unoptimized: true,
+    path: `${process.env.NEXT_PUBLIC_BASE_PATH || ''}`,
   },
+  trailingSlash: true, // GitHub Pages에서 필요할 수 있습니다.
 }
 
 module.exports = nextConfig
