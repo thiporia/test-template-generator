d140fe4e66ea73dd418ec3cc0a07f408a4d6ee69
Seonyeong Jang <ssoju2827@gmail.com>
2024-07-01 23:49:35 +0900

    update yml and configuration

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const isProduction = process.env.NODE_ENV === 'production';
+const repoName = 'test-template-generator'; // GitHub 저장소 이름으로 변경하세요
 
 const nextConfig = {
-  assetPrefix: isProduction ? '/test-template-generator' : '/',
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
