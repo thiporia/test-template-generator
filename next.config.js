1f44f69f7c11ada9988dd298352f067c1817f5ac
Seonyeong Jang <ssoju2827@gmail.com>
2024-07-01 23:51:05 +0900

    fix next configuration

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const isProduction = process.env.NODE_ENV === 'production';
 const repoName = 'test-template-generator'; // GitHub 저장소 이름으로 변경하세요
 
 const nextConfig = {
+  assetPrefix: isProduction ? `/${repoName}/` : '/',
+  basePath: isProduction ? `/${repoName}` : '',
   output: 'export',
   images: {
     unoptimized: true,
