f9c2c9afac0d61d4b8e8362b03c47c693ca15de7
Seonyeong Jang <ssoju2827@gmail.com>
2024-07-02 00:22:51 +0900

    remove gh-pages configurations

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
-  images: {
-    unoptimized: true,
-    path: `${process.env.NEXT_PUBLIC_BASE_PATH || ''}`,
-  },
-  trailingSlash: true, // GitHub Pages에서 필요할 수 있습니다.
+  
 }
 
 module.exports = nextConfig
